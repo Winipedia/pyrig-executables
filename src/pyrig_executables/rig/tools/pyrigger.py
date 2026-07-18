caa624c237e1c862b742e5b6333061abb235ed05
Winipedia <winipedia@gmx.de>
2026-07-18 02:40:35 +0100

    moved second sync call to after second install

--- src/pyrig_executables/rig/tools/pyrigger.py
+++ src/pyrig_executables/rig/tools/pyrigger.py
@@ -4,6 +4,7 @@ from typing import Any
 
 from pyrig.core.subprocesses import Args
 from pyrig.rig.cli.subcommands import sync
+from pyrig.rig.tools.packages.manager import PackageManager
 from pyrig.rig.tools.pyrigger import Pyrigger as BasePyrigger
 
 
@@ -14,8 +15,10 @@ class Pyrigger(BasePyrigger):
         """Override this method to customize the setup steps for the Pyrigger tool."""
         steps = list(super().setup_steps())
         sync_args = self.cmd_args(cmd=sync)
+        sync_step = next((args, kwargs) for args, kwargs in steps if args == sync_args)
+        second_install_args = PackageManager.I.install_dependencies_args()
         index = next(
-            (i for i, (args, _) in enumerate(steps) if args == sync_args),
+            (i for i, (args, _) in enumerate(steps) if args == second_install_args),
         )
-        steps.insert(index + 1, steps[index])
+        steps.insert(index + 1, sync_step)
         return tuple(steps)
