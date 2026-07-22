9f6cad2734b3fce908f582db3074253cfb3da6b1
Winipedia <winipedia@gmx.de>
2026-07-22 19:14:24 +0100

    test fix

--- tests/test_pyrig_executables/test_rig/test_cli/test_subcommands.py
+++ tests/test_pyrig_executables/test_rig/test_cli/test_subcommands.py
@@ -1,6 +1,6 @@
 """Test module."""
 
-from collections.abc import Callable
+from collections.abc import Callable, Iterable
 from types import FunctionType
 
 from pyrig_executables.rig.cli.commands.run import run_main
@@ -9,8 +9,8 @@ from pyrig_executables.rig.cli.subcommands import run
 
 def test_run(
     command_works: Callable[[FunctionType], bool],
-    command_calls_function: Callable[[FunctionType, FunctionType], bool],
+    command_calls_function: Callable[[FunctionType, FunctionType, Iterable[str]], bool],
 ) -> None:
     """Test function."""
     assert command_works(run)
-    assert command_calls_function(run, run_main)
+    assert command_calls_function(run, run_main, [])
